ad7db946b3c82afda79fb025cadd1c6f93c9ca24
Kraken Github <KrakenWebDev@gmail.com>
2015-10-27 21:48:18 -0700

    styling updates for splash

---
+++ slackr.js
@@ -0,0 +1,10 @@
+(function(win, doc, undefined) {
+
+    // Cache DOM Selectors
+    var modal     = doc.getElementById('overlay');
+    var overlay   = doc.getElementById('overlay');
+    var closeBtn  = doc.getElementById('close-modal');
+    var input     = doc.getElementById('search-input');
+    var searchBtn = doc.getElementById('slackr-button');
+
+})(window, document);
